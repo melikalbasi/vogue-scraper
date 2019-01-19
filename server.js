4b321557cc5ac0fc671f8d6a77d787f811a3fe70
Melika Kalbasi <melikalbasi@gmail.com>
2019-01-18 17:37:43 -0800

    require dependencies in server.js

---
+++ server.js
@@ -0,0 +1,60 @@
+var express = require("express"); 
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars")
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+// Initialize Express
+var app = express();
+
+// Set Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Routes
+app.get("/", function(req, res) {
+  res.render("home");
+  console.log("connected");
+})
+
+
+app.get("/saved", function(req, res) {
+  res.render("saved");
+})
+
+app.listen(PORT, function() {
+  // Log server-side when our server has started
+  console.log("Server is listening on: http://localhost:" + PORT);
+});
